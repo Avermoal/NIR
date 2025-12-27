9239ec67ca3b2914132437e02ac2f7a2bf2cb070
Avermoal <misho54@mail.ru>
2025-12-27 19:30:54 +0600

    Add BeH2.tar.gz

--- BeH/results/get_potential_energy.sh
+++ BeH/results/get_potential_energy.sh
@@ -6,7 +6,7 @@ echo "set terminal pdf
 set output 'BeH_potenergy_LDA.pdf'
 set xlabel 'Interatomic distance, Angstrom'
 set ylabel 'Potential energy, eV'
-set key right top
+set key center top
 set key font 'Arial, 7'
 plot 'beh-e-v-LDA-200.txt' w lp title 'ENCUT = 200', 'beh-e-v-LDA-250.txt' w lp title 'ENCUT = 250', 'beh-e-v-LDA-300.txt' w lp title 'ENCUT = 300', 'beh-e-v-LDA-350.txt' w lp title 'ENCUT = 350', 'beh-e-v-LDA-400.txt' w lp title 'ENCUT = 400', 'beh-e-v-LDA-450.txt' w lp title 'ENCUT = 450', 'beh-e-v-LDA-500.txt' w lp title 'ENCUT = 500'," | gnuplot
 
@@ -14,6 +14,6 @@ echo "set terminal pdf
 set output 'BeH_potenergy_PBE.pdf'
 set xlabel 'Interatomic distance, Angstrom'
 set ylabel 'Potential energy, eV'
-set key right top
+set key center top
 set key font 'Arial, 7'
 plot 'beh-e-v-PBE-200.txt' w lp title 'ENCUT = 200', 'beh-e-v-PBE-250.txt' w lp title 'ENCUT = 250', 'beh-e-v-PBE-300.txt' w lp title 'ENCUT = 300', 'beh-e-v-PBE-350.txt' w lp title 'ENCUT = 350', 'beh-e-v-PBE-400.txt' w lp title 'ENCUT = 400', 'beh-e-v-PBE-450.txt' w lp title 'ENCUT = 450', 'beh-e-v-PBE-500.txt' w lp title 'ENCUT = 500'," | gnuplot
